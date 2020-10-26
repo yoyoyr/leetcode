96bb098f85ca74735ac6f7592e060f5d148ebc85
杨荣 <296550494@qq.com>
2020-10-26 14:13:24 +0800

    回文字符串判断

---
+++ libLeetCode/src/main/java/com/libleetcode/Huiwen.java
@@ -0,0 +1,21 @@
+package com.libleetcode;
+
+public class Huiwen {
+
+    static final String datas = "A man, a plan, a canal: Panama";
+
+    public static void main(String[] args) {
+        String tmp = datas.toLowerCase().replaceAll("[^0-9a-z]", "");
+//        System.out.println("tmp = "+tmp);
+        int point1 = 0;
+        int point2 = tmp.length() - 1;
+        while (point1 < point2) {
+            if (tmp.charAt(point1++) != tmp.charAt(point2--)) {
+                System.out.println("false");
+                return;
+            }
+        }
+
+        System.out.println("true");
+    }
+}
